28507e08afc78e1848368597a94ab23b757722fe
orekihoutaro04 <38377260+orekihoutaro@users.noreply.github.com>
2020-08-08 21:08:54 +0800

    Static Web Files for Flask Templates. Migration of Web Static to Flask App.

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+'''
+    Documentation v1.0.0
+
+    Author: Jomer Ventolero
+    Email: orekihoutaro1218@gmail.com
+    Company: Adarna Reborn Game Dev
+
+'''
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
