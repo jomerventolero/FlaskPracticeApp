bc4d38f4500b046f4807b08583229b8b63b2c50c
orekihoutaro04 <38377260+orekihoutaro@users.noreply.github.com>
2020-08-11 10:27:18 +0800

    Changed to IDE now using Pycharm. Trying to commit.

--- main.py
+++
@@ -1,9 +0,0 @@
-import pygame as pg
-
-
-class Main:
-    def __init__(self, width, height):
-        pg.init()
-        self.width = width
-        self.height = height
-        pg.display.set_mode(self.width, self.height)
